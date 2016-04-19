160911ceff9b228d2b7203b30507bd8447bb0aae
Thineskumar Thilakanathan <skwebbies@hotmail.co.uk>
2016-04-19 19:08:39 +0200

    uploads

---
+++ Uppgift/Uppgifter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift
+{
+    //Uppgift1
+   public class Uppgifter
+    {
+        public string ConcatArrayWithSpace(string[] array)
+        {
+
+            return String.Join(" ", array);
+        }
+        public string Capitalize(string str)
+        {
+            
+            string ret = new String(str.Where(c => c != '-' && (c < '0' || c > '9')).ToArray());
+
+            ret = UppercaseWords(ret);
+
+            return (ret.Replace( '.', '_').Replace(',', '_').Replace(';', '_').Replace(':', '_').Replace('!', '_').Replace('?', '_').Replace('`', '_').Replace('"', '_').Replace('-', '_').Replace('/', '_').Replace('(', '_').Replace(')', '_').Replace('[', '_').Replace(']', '_').Replace('*', '_').Replace(" ", ""));
+            
+
+        }
+        public string UppercaseFirstEach(string str)
+        {
+            return (UppercaseWords(str));
+        }
+        public int Average(int tal1, int tal2)
+        {
+            return (tal1 + tal2) / 2;
+            
+        }
+
+        static string UppercaseWords(string value)
+        {
+            char[] array = value.ToCharArray();
+            
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+             for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == ' ')
+                {
+                    if (char.IsLower(array[i]))
+                    {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+    }
+}
+
---
+++ UppgiftTest/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uppgift;
+using System.Text.RegularExpressions;
+
+namespace UppgiftTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        
+        [TestMethod]
+        public void ConcatArrayWithSpace()
+        {
+            Uppgifter up = new Uppgifter();
+
+            string[] str = new string[] { "String1", "String2", "String3" };
+
+
+            var act = up.ConcatArrayWithSpace(str);
+            Assert.IsInstanceOfType(act, typeof(string));
+            TestContext.WriteLine(act);
+
+        }
+        [TestMethod]
+        public void Capitalize()
+        {
+            Uppgifter up = new Uppgifter();
+
+            string str = "test1 test2 test3.,:";
+
+
+            var act = up.Capitalize(str);
+
+            Regex number = new Regex(@"[0-9]+$");
+
+            Assert.IsTrue(act.IsUpper() && act.Contains("_") && !act.Contains(" ") && !number.IsMatch(act));
+            TestContext.WriteLine(act);
+
+        }
+        [TestMethod]
+        public void UppercaseFirstEach()
+        {
+            Uppgifter up = new Uppgifter();
+
+            string str = "test test test";
+
+            var act = up.UppercaseFirstEach(str);
+
+            Assert.IsTrue(act.IsUpper());
+            TestContext.WriteLine(act);
+
+        }
+        [TestMethod]
+        public void Avrage()
+        {
+            Uppgifter up = new Uppgifter();
+
+            int tal1 = 8;
+
+            int tal2 = 6;
+
+            int avg = 7;
+
+            var act = up.Average(tal1, tal2);
+            Assert.AreEqual(act, avg, "result is not equal to expected value" + avg);
+            TestContext.WriteLine(act.ToString());
+
+
+        }
+
+
+    }
+
+    static class Extensions
+    {
+        public static bool IsUpper(this string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+
+                if (char.IsUpper(value[i]))
+
+                    return true;
+
+            return false;
+        }
+    }
+}
